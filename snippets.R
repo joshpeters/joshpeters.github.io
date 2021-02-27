21f4d1a67af982805e6322c756d63e64ba4da5f3
joshpeters <petersjm@mit.edu>
2021-02-27 11:25:44 -0500

    biotech. etf post

--- snippets.R
+++ snippets.R
@@ -2,5 +2,5 @@ library(distill)
 library(rmarkdown)
 render_site()
 
-?distill::create_post("Test", draft = TRUE)
+distill::create_post("venture-capital", draft = TRUE)
 distill::rename_post_dir(post_dir = "_posts/2021-02-18-change")
