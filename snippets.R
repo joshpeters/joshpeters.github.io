802fe7ab1db154c0392544d40e73f471d6a3658c
joshpeters <petersjm@mit.edu>
2021-03-06 09:33:25 -0500

    update

--- snippets.R
+++ snippets.R
@@ -2,5 +2,5 @@ library(distill)
 library(rmarkdown)
 render_site()
 
-distill::create_post("venture-capital", draft = TRUE)
+distill::create_post("upcoming", draft = FALSE)
 distill::rename_post_dir(post_dir = "_posts/2021-02-18-change")
